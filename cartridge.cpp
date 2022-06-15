a8ad82ee5444fa1d22a04710791c781573cda836
Charlie Birks <charlie@daft.games>
2022-06-15 14:09:07 +0100

    PIO cart reading
    Now up to 511 kB/s

--- cartridge.cpp
+++ cartridge.cpp
@@ -4,6 +4,7 @@
 #include "hardware/pio.h"
 
 #include "cartridge.hpp"
+#include "gba-cart.pio.h"
 
 // 24 bit addr on pins 0-23
 // 16 bit data on pins 0-15
@@ -15,15 +16,56 @@ static const int ramCSPin = 27;
 
 namespace Cartridge
 {
+    int pioSM = -1;
+
+    static void gba_multi_program_init(PIO pio, uint sm, uint offset)
+    {
+        int basePin = 0;
+        int addressPins = 24;
+        int dataPins = 16;
+
+        uint32_t sideMask = 1 << wrPin | 1 << rdPin | 1 << romCSPin | 1 << ramCSPin;
+        uint32_t pinMask = sideMask | ((1 << dataPins) - 1);
+
+        pio_sm_set_pins_with_mask(pio, sm, sideMask, pinMask); // set all control pins inactive
+        pio_sm_set_pindirs_with_mask(pio, sm, pinMask, pinMask); // set all pins as output
+
+        for(int i = 0; i < 16; i++)
+            pio_gpio_init(pio, basePin + i);
+
+        pio_gpio_init(pio, wrPin);
+        pio_gpio_init(pio, rdPin);
+        pio_gpio_init(pio, romCSPin);
+        pio_gpio_init(pio, ramCSPin);
+
+        pio_sm_config c = gba_cart_program_get_default_config(offset);
+
+        sm_config_set_in_shift(&c, true, true, 16);
+        sm_config_set_out_shift(&c, true, false, 32);
+
+        sm_config_set_in_pins(&c, basePin);
+        sm_config_set_out_pins(&c, basePin, dataPins);
+        sm_config_set_sideset_pins(&c, basePin + addressPins);
+
+        // default wait states for cart ROM are 4/2, most games use 3/1
+        // ... so we can get a few MHz
+        sm_config_set_clkdiv_int_frac(&c, 15, 0);
+
+        pio_sm_init(pio, sm, offset, &c);
+        pio_sm_set_enabled(pio, sm, true);
+    }
+
     void initIO()
     {
-        // init all IOs
-        auto mask = (1 << 28) - 1;
+        // init PIO
+        uint offset = pio_add_program(pio0, &gba_cart_program);
+        pioSM = pio_claim_unused_sm(pio0, true);
+        gba_multi_program_init(pio0, pioSM, offset);
+
+        // init high address bits (PIO doesn't control these)
+        auto mask = 0xFF << 16;
         gpio_init_mask(mask);
         gpio_set_dir_out_masked(mask);
-
-        // inactive
-        gpio_put_all(1 << wrPin | 1 << rdPin | 1 << romCSPin | 1 << ramCSPin);
     }
 
     // read func
@@ -32,36 +74,14 @@ namespace Cartridge
         assert((addr & 1) == 0);
         assert((addr & 0xFFFF) + count <= 0xFFFF);
 
-        auto addrMask = (1 << 24) - 1;
-        auto dataMask = (1 << 16) - 1;
+        // write high bits of address
+        gpio_put_masked(0xFF << 16, addr >> 1);
 
-        // write address
-        gpio_put_masked(addrMask, addr >> 1);
-
-        const int delay = 1;
-        sleep_us(delay);
-        
-        // cs active
-        gpio_put(romCSPin, 0);
-        sleep_us(delay);
-
-        // set pins to input
-        gpio_set_dir_in_masked(dataMask);
+        // count and low bits of address
+        pio_sm_put_blocking(pio0, pioSM, count - 1);
+        pio_sm_put_blocking(pio0, pioSM, (addr >> 1) & 0xFFFF);
 
         while(count--)
-        {
-            // rd active
-            gpio_put(rdPin, 0);
-            sleep_us(delay);
-
-            *data++ = gpio_get_all() & dataMask;
-
-            gpio_put(rdPin, 1);
-            sleep_us(delay);
-        }
-
-        gpio_put(romCSPin, 1);
-        gpio_set_dir_out_masked(dataMask);
-        sleep_us(delay);
+            *data++ = pio_sm_get_blocking(pio0, pioSM) >> 16;
     }
 }
