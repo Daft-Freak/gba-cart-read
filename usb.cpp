41bf5516ed79dff6312117b6b8cbd8e3f158cbf0
Charlie Birks <charlie@daft.games>
2022-06-16 11:54:46 +0100

    Return not ready/no media for empty FS

--- usb.cpp
+++ usb.cpp
@@ -53,7 +53,7 @@ void tud_msc_inquiry_cb(uint8_t lun, uint8_t vendor_id[8], uint8_t product_id[16
 
 bool tud_msc_test_unit_ready_cb(uint8_t lun)
 {
-    if(storageEjected)
+    if(storageEjected || Filesystem::getNumSectors() == 0)
     {
         tud_msc_set_sense(lun, SCSI_SENSE_NOT_READY, 0x3a, 0x00);
         return false;
